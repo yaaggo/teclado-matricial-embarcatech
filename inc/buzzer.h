094e9cebf17b5b720606d5bbc26d2e16fb75e07a
Buurunu <BurunnuFlores@hotmail.com>
2025-01-19 01:51:39 -0300

    adição do cabeçalho

--- inc/buzzer.h
+++ inc/buzzer.h
@@ -1,10 +1,15 @@
 #ifndef BUZZER_H
 #define BUZZER_H
-
+#include <stdint.h>
 // Macros
 
 #define BUZZER_A_PIN 21
 
 // Cabeçalhos
+void buzzer_set_frequency(uint pin, uint frequency);
+void buzzer_play(uint pin, uint frequency);
+void buzzer_stop(uint pin);
+
+
 
 #endif
