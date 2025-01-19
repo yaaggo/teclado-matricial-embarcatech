e4b04a41b0f1bdb4aa515f01e1913a6ab4ec321f
Camila-Miranda <camilasilveirabm@gmail.com>
2025-01-19 12:30:55 -0300

    Alteração da funçoes

--- inc/led.c
+++ inc/led.c
@@ -1,3 +1,28 @@
 #include "led.h"
+#include "pico/stdlib.h"
 
-// Funções
+
+
+// Função para inicializar um LED
+void led_init(uint gpio) {
+    gpio_init(gpio);
+    gpio_set_dir(gpio, GPIO_OUT);
+    gpio_put(gpio, 0); // Inicialmente desligado
+}
+
+// Função para ligar um LED
+void led_tunr_on(uint gpio) {
+    gpio_put(gpio, 1);
+}
+
+// Função para desligar um LED
+void led_turn_off(uint gpio) {
+    gpio_put(gpio, 0);
+}
+
+// Função para desligar todos os LEDs
+void leds_turn_off_all() {
+    turn_off_led(LED_RED_PIN);
+    turn_off_led(LED_GREEN_PIN);
+    turn_off_led(LED_BLUE_PIN);
+}
